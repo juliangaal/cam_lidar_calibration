18da32e42d335ce7870aa0d08e6a66c497ba541e
Julian Gaal <julian.gaal@anavs.de>
2023-06-02 19:22:28 +0200

    exit early and clear during assessment

---
+++ include/cam_lidar_calibration/filesystem.h
@@ -0,0 +1,8 @@
+//
+// Created by julian on 02.06.23.
+//
+
+#ifndef SRC_FILESYSTEM_H
+#define SRC_FILESYSTEM_H
+
+#endif //SRC_FILESYSTEM_H
---
+++ include/cam_lidar_calibration/node_handle.h
@@ -0,0 +1,8 @@
+//
+// Created by julian on 02.06.23.
+//
+
+#ifndef SRC_NODE_HANDLE_H
+#define SRC_NODE_HANDLE_H
+
+#endif //SRC_NODE_HANDLE_H
